008950e1456c97bc5087d8282ced37e1de1a6706
itzisurii <itzIsurika08@gmail.com>
2026-02-08 16:06:30 +0530

    Add BeveragesComponent with initial structure and metadata

---
+++ src/app/components/beverages/beverages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beverages',
+  imports: [],
+  templateUrl: './beverages.component.html',
+  styleUrl: './beverages.component.css'
+})
+export class BeveragesComponent {
+
+}
