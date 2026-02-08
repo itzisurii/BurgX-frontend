a9cf8f18ab6db415e27447f9e09e8fb079c90d4a
itzisurii <itzIsurika08@gmail.com>
2026-02-08 16:05:35 +0530

    Add component imports to AppComponent for enhanced functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from "./components/top-bar/top-bar.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { BurgerGridComponent } from "./components/burger-grid/burger-grid.component";
+import { OrderSummaryComponent } from "./components/order-summary/order-summary.component";
+import { BeveragesComponent } from "./components/beverages/beverages.component";
+import { OrderCartComponent } from "./components/order-cart/order-cart.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent, NavBarComponent, BurgerGridComponent, OrderSummaryComponent, BeveragesComponent, OrderCartComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
