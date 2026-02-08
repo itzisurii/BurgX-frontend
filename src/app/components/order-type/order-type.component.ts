18cec60c0cf8c90637a3a442844093eb97b2c24c
itzisurii <itzIsurika08@gmail.com>
2026-02-08 16:20:39 +0530

    Add OrderTypeComponent with initial structure and metadata

---
+++ src/app/components/order-type/order-type.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-type',
+  imports: [],
+  templateUrl: './order-type.component.html',
+  styleUrl: './order-type.component.css'
+})
+export class OrderTypeComponent {
+
+}
