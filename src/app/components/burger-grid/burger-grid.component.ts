a05b1c6870483abd69bab1b36055967ae359fb37
itzisurii <itzIsurika08@gmail.com>
2026-02-08 16:07:43 +0530

    Add BurgerGridComponent with initial structure and metadata

---
+++ src/app/components/burger-grid/burger-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-burger-grid',
+  imports: [],
+  templateUrl: './burger-grid.component.html',
+  styleUrl: './burger-grid.component.css'
+})
+export class BurgerGridComponent {
+
+}
