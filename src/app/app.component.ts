f0bcaa07037d116c8aa4d19a8d8e315a333f21b7
itzisurii <itzIsurika08@gmail.com>
2026-02-11 14:38:28 +0530

    Remove unused component imports from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from "./components/top-bar/top-bar.component";
-import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
-import { BurgerGridComponent } from "./components/burger-grid/burger-grid.component";
-import { OrderSummaryComponent } from "./components/order-summary/order-summary.component";
-import { BeveragesComponent } from "./components/beverages/beverages.component";
-import { OrderCartComponent } from "./components/order-cart/order-cart.component";
-import { HomeComponent } from "./pages/home/home.component";
+import {RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent, NavBarComponent, BurgerGridComponent, OrderSummaryComponent, BeveragesComponent, OrderCartComponent, HomeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
