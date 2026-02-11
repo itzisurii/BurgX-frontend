ecc963dcb45d99c2f227958f523ff9ddea90796c
itzisurii <itzIsurika08@gmail.com>
2026-02-11 20:20:57 +0530

    Refactor home component to include additional imports and enhance active view handling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { OrderCartComponent } from "../../components/order-cart/order-cart.component";
-import { TopBarComponent } from "../../components/top-bar/top-bar.component";
-import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { BurgerGridComponent } from "../../components/burger-grid/burger-grid.component";
 import { BeveragesComponent } from "../../components/beverages/beverages.component";
 import { OrderSummaryComponent } from "../../components/order-summary/order-summary.component";
 import { OrderTypeComponent } from "../../components/order-type/order-type.component";
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { AllOrdersComponent } from "../../components/all-orders/all-orders.component";
+import { CommonModule } from '@angular/common';
+import { DineInComponent } from "../../components/dine-in/dine-in.component";
 
 @Component({
   selector: 'app-home',
-  imports: [OrderCartComponent, TopBarComponent, NavBarComponent, BurgerGridComponent, BeveragesComponent, OrderSummaryComponent, OrderTypeComponent],
+  imports: [CommonModule, OrderCartComponent, NavBarComponent, BurgerGridComponent, BeveragesComponent, OrderSummaryComponent, OrderTypeComponent, AllOrdersComponent, DineInComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  activeView: string = 'dashboard';
 
+  handlePanelChange(viewName: string) {
+    console.log('Switching to:', viewName);
+    this.activeView = viewName;
+  }
 }
