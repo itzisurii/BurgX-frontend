fe76fc140b56e9b2280dfc1514351c865a411ae8
itzisurii <itzIsurika08@gmail.com>
2026-02-15 17:01:05 +0530

    Add TakeawayComponent with initial structure and metadata

---
+++ src/app/components/takeaway/takeaway.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-takeaway',
+  imports: [],
+  templateUrl: './takeaway.component.html',
+  styleUrl: './takeaway.component.css'
+})
+export class TakeawayComponent {
+
+}
