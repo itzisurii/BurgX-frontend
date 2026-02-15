a268304ec9e02b424ecbd9d9bb29b8b3a43b5d48
itzisurii <itzIsurika08@gmail.com>
2026-02-15 17:01:55 +0530

    Add TakeawaysidebarComponent with initial structure

---
+++ src/app/components/takeawaysidebar/takeawaysidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-takeawaysidebar',
+  imports: [],
+  templateUrl: './takeawaysidebar.component.html',
+  styleUrl: './takeawaysidebar.component.css'
+})
+export class TakeawaysidebarComponent {
+
+}
