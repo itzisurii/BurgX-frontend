bd62cf05fb5db7b460a8d3d3a40be609cee6658f
itzisurii <itzIsurika08@gmail.com>
2026-02-17 10:30:20 +0530

    Implement smooth scrolling functionality in BurgerGridComponent

--- src/app/components/burger-grid/burger-grid.component.ts
+++ src/app/components/burger-grid/burger-grid.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-burger-grid',
-  imports: [],
+  standalone: true,
   templateUrl: './burger-grid.component.html',
   styleUrl: './burger-grid.component.css'
 })
 export class BurgerGridComponent {
+
+  scrollToSection(sectionId: string) {
+  const element = document.getElementById(sectionId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
 
 }
